200c0ae91bdeab3cf2ceef5796b40aea03285b7f
Beth Smith <bethsmith0623@gamil.com>
2019-08-05 09:55:49 -0500

    adds constants and state

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*---- constants ---*/
+const BEGBALANCE = "$100"
+
+const CHIP = new Image ("Images/bluePokerChip.png")
+/*---- app's state (variables) ----*/
+let wager, balance, selection;
+
+/*---- cached element references ----*/
+
+
+/*---- event listeners ----*/
+
+
+/*---- functions ----*/
+init();
+
+function initialize(){
+  wager = '$0';
+  balance = '$100';
+};
