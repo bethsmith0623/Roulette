f3a2b40c3307b9f790a1df3d7a075987a75ca800
Beth Smith <bethsmith0623@gamil.com>
2019-08-06 11:15:27 -0500

    adds next spin button

--- js/script.js
+++ js/script.js
@@ -4,38 +4,40 @@
 let bet, bal, selection;
 
 /*---- cached element references ----*/
-let newBal = document.getElementById("bal").value;
-let betBtn = document.getElementById("bet").value;
-
+let bal2 = document.getElementById("bal");
+let bet2 = document.getElementById("bet");
 
 /*---- event listeners ----*/
 document.getElementById("placeBet").addEventListener('click', makeBet);
 document.querySelector(".table").addEventListener('click', selectNum);
 document.getElementById("spin").addEventListener('click', spinWhl);
+document.getElementById("nextSpin").addEventListener('click', reset);
 
 /*---- functions ----*/
 initialize();
 
 function initialize(){
-  bal;
+  // bal = 100;
   bet = 100;
-  document.getElementById("bal").textContent = bet;
+  document.getElementById("bal").value = bet;
 };
 
 function makeBet() {
-  let betBtn = document.getElementById("bet").value;
+  let bet3 = document.getElementById("bet").value;
   document.getElementById("message").innerHTML = 
-  `The player's bet is $ ${betBtn}. Make your table selection.`;
+  `The player's bet is $ ${bet3}. Make your table selection.`;
 };
 
 function selectNum(evt) {
-  console.log(` ${evt.target.innerHTML}`);
+  console.log(`${evt.target.innerHTML}`);
   evt.target.style.border = '5px solid blue';
+  document.getElementById("spin").style.backgroundColor = "green";
 };
 
 function spinWhl() {
   let spinVal = (Math.floor(Math.random() * (36-0) + 0));
   document.getElementById("whlNum").innerHTML = spinVal
+  document.getElementById("spin").style.backgroundColor = "white";
   // last user action
   // evaluate the values of bet vs. spin
   matchSpin(selection, spinVal)
@@ -43,20 +45,29 @@ function spinWhl() {
 
 function matchSpin(guess, actual) {
   if (guess === actual) {
+    document.getElementById("message").style.color = "green"
     document.getElementById("message").innerHTML = "You WIN!"
   } else {
+    document.getElementById("message").style.color = "red"
     document.getElementById("message").innerHTML = "You lose"
   }
-  getBalance(bal, betBtn);
+  document.getElementById("nextSpin").style.backgroundColor = "green"
+  getBalance(bal2, bet2);
 };
 
 function getBalance(guess, actual) {
-  let newBal = document.getElementById("bal").value;
-  console.log(`The player bet ${betBtn}`)
-  console.log(`the players new total is ${newBal}`)
+  // let newBal = document.getElementById("bal").value;
+  // console.log(`The player bet ${bet2.value}`)
+  // console.log(`the players new total is ${bal2}`)
   if (guess !== actual) {
-    return (newBal - betBtn);
+    console.log(bal2.value - bet2.value);
   } else {
-    return (newBal + betBtn);
+    console.log(bal2.value + bet2.value);
   }
 };
+
+function reset() {
+  document.getElementById("nextSpin").style.backgroundColor = "white"
+  bet = '';
+  
+}
